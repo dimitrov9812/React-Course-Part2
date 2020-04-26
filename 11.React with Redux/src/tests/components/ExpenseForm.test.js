44ee4e713c336107e372f831e95ae4c8c000af5b
dimitrov9812 <alexanderdimitrov333@gmail.com>
2020-04-26 21:17:48 +0300

    Learned we can access the state of the components and learned how to simulate actions such as button click, submit or change. Did a couple of tests to practice the new things learned.

--- 11.React with Redux/src/tests/components/ExpenseForm.test.js
+++ 11.React with Redux/src/tests/components/ExpenseForm.test.js
@@ -20,3 +20,74 @@ test('Should render ExpenseForm with expense data', () =>{
     const wrapper = shallow(<ExpenseForm expense = {expenses[0]}/>);
     expect(toJSON(wrapper)).toMatchSnapshot();
 });
+
+// we will simulate click with this test
+test('shoul render ERROR for invalid form submission', () => {
+    const wrapper = shallow(<ExpenseForm />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+
+    wrapper.find('form').simulate('submit',{
+        preventDefault: () => {}
+    });
+    expect(wrapper.state('error').length).toBeGreaterThan(0);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+})
+
+test("should set description on input change", () => {
+    const value = 'New description'
+    const wrapper = shallow(<ExpenseForm />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+
+    wrapper.find('input').at(0).simulate('change',{
+        target: { value }
+    }) 
+    expect(wrapper.state('description')).toBe(value);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+});
+
+// ============Challenge=============
+// 1. Should set note on Text area change
+
+test("should set note on text change", () => {
+    const value = 'New Note'
+    const wrapper = shallow(<ExpenseForm />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+
+    wrapper.find('textarea').simulate('change',{
+        target: { value }
+    }) 
+    expect(wrapper.state('note')).toBe(value);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+});
+
+// 2. Shoud set amount if valid input
+// Example: 23.50
+
+test("should set amount if valid input", () => {
+    const value = '23.50'
+    const wrapper = shallow(<ExpenseForm />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+
+    wrapper.find('input').at(1).simulate('change',{
+        target: { value }
+    })
+
+    expect(wrapper.state('amount')).toBe(value);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+});
+
+// 3. Shound not set amount if invalid input
+// Example: 12.122
+
+test("should not set amount if invalid input", () => {
+    const value = '23.50323'
+    const wrapper = shallow(<ExpenseForm />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+
+    wrapper.find('input').at(1).simulate('change',{
+        target: value
+    })
+
+    expect(wrapper.state('amount')).toBe(undefined);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+});
