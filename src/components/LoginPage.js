239afb44b735bab7081234e9719dc6174c9f7625
dimitrov9812 <alexanderdimitrov333@gmail.com>
2020-05-07 20:16:18 +0300

    finished

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,8 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => (
+    <div>
         <div className="box-layout">
-            <div className="wrap">
                 <div className="login-div">
                     <div clasName="login-text-div">
                         <p className="login-text">Login</p>
@@ -14,19 +14,21 @@ export const LoginPage = ({ startLogin }) => (
                             <p className="user-guide">Username</p>
                             <input className="login-input" type="text" placeholder="Email"/><br/>
                         </div>
-                        <div className="input">
+                        <div className="input pass-input">
                             <p className="user-guide">Password</p>
                             <input className="login-input" type="password" placeholder="Password"/>
                         </div>
+                        <button className="login-button" onClick={startLogin}>Login</button>
                     </div>
-                    <button className="login-button" onClick={startLogin}>Login</button>
                 </div>
-                <div className="after-from">
+                <div className="final">
                     <h3 className="item">Don't have an account?</h3>
                     <h4 id="signUp-item">SignUp</h4>
                 </div>
-            </div>
         </div>
+    </div>
+        
+
     
 );
 
